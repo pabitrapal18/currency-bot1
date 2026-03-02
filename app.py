191dd68d2cf47e5a1126504a1aa910c435d19710
pabitrapal18 <pabitrapal18aug@gmail.com>
2026-03-02 20:01:16 +0530

    final app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import requests
-
+import os
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
@@ -40,4 +40,6 @@ def fetch_conversion_factor(source_currency, target_currency):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
